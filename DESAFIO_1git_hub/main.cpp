48f56192d42b6a78276ce7b313adb750ed86d1e9
Emmanuelgt-code <emmanuel.guerrat@udea.edu.co>
2026-03-22 00:07:24 -0500

    Modifique bajar, agregue las colisiones, borrar filas cambie imprimirTablero

--- DESAFIO_1git_hub/main.cpp
+++ DESAFIO_1git_hub/main.cpp
@@ -4,17 +4,21 @@ using namespace std;
 #include <ctime>
 
 // PROTOTIPOS
-void imprimirTablero(unsigned char** ptr, unsigned int filas, unsigned int columnas);
+void imprimirTablero(unsigned char** ptr, unsigned int filas, unsigned int columnas, unsigned char** tablerofijo);
 void inicializarTablero(unsigned char** ptr, unsigned int filas, unsigned int columnas);
 void crearMatriz(unsigned char** ptr, unsigned int filas, unsigned int columnas);
-void ingresarFicha(unsigned char** ptr, unsigned char piezas[5][4],unsigned int columnas,unsigned int columnas_bits);
-void bajar(unsigned char** ptr,unsigned int &filaFicha,unsigned int columnas_bits,unsigned int filas);
-void MoverDerecha(unsigned char** ptr,unsigned int filaFicha,unsigned int columnas_bits,unsigned int filas);
-void MoverIzquierda(unsigned char** ptr,unsigned int filaFicha,unsigned int columnas_bits);
+void ingresarFicha(unsigned char** ptr, unsigned char piezas[5][4],unsigned int columnas, unsigned int columnas_bits,unsigned int &filaficha,unsigned int &dimensiony,unsigned char** tablerofijo,bool &juegoActivo);
+void bajar(unsigned char** ptr,unsigned char** tablerofijo,unsigned int &filaFicha,unsigned int columnas_bits,unsigned int filas, unsigned char piezas[5][4],unsigned int columnas,unsigned int &dimensiony,bool &juegoActivo);
+void MoverDerecha(unsigned char** ptr,unsigned int filaFicha,unsigned int columnas_bits,unsigned int filas,unsigned int dimensiony);
+void MoverIzquierda(unsigned char** ptr,unsigned int filaFicha,unsigned int columnas_bits,unsigned int dimensiony);
+bool hayColisionAbajo(unsigned char** ptr,unsigned char** tablerofijo,unsigned int filaFicha,unsigned int columnas_bits,unsigned int filas, unsigned int dimensiony);
+void borrarFilas(unsigned char** tablerofijo, unsigned int filas, unsigned int columnas_bits);
+
 int main()
 {
-    unsigned int filas,columnas,columnas_bits,filaFicha = 0;// filas
+    unsigned int filas,columnas,columnas_bits,dimensiony = 0,filaFicha = 0,posicionx = 0;// filas
     unsigned char** ptr;
+    unsigned char** tablerofijo;
     bool juegoActivo = true;
     cout << "numero del tamaño de las filas: " << endl;
     cin >> filas;
@@ -22,22 +26,28 @@ int main()
     cin >> columnas;
     columnas_bits = columnas/8;
     ptr = new unsigned char*[filas];
+    tablerofijo = new unsigned char*[filas];
+    //piezas
     unsigned char piezas[5][4] = // creo una matriz con todas las fichas a nivel de columnas_bits
         {
-            {0b11110000,0,0,0},      // I
+            {0b10000000,0b10000000,0b10000000,0b10000000},      // I
             {0b11100000,0b01000000,0,0}, // T
             {0b11000000,0b00110000,0,0}, // Z
-            {0b11100000,0b10000000,0,0}, // L
+            {0b10000000,0b10000000,0b11000000,0}, // L
             {0b11000000,0b11000000,0,0}  // O
         };
 
     //inicializo la aleatoridad
     srand(time(0));
-    // Aqui inicializamos y creamos la matriz
+    // Aqui inicializamos y creamos la matriz que maneja la ficha
     crearMatriz(ptr,filas,columnas_bits);
     inicializarTablero(ptr,filas,columnas_bits);
+
+    // creo el tablero donde se guarden las fichas
+    crearMatriz(tablerofijo,filas,columnas_bits);
+    inicializarTablero(tablerofijo,filas,columnas_bits);
     // Agregamos la primer ficha
-    ingresarFicha(ptr, piezas, columnas, columnas_bits);
+    ingresarFicha(ptr,piezas,columnas,columnas_bits,filaFicha,dimensiony,tablerofijo,juegoActivo);
 
     cout << "\n=======================================================\n";
     cout << "  _____     _       _      __  __           _   _ \n";
@@ -49,7 +59,7 @@ int main()
     while(juegoActivo){
         system("cls"); // limpia la pantalla
         // Mostrar el tablero en cada iteración
-        imprimirTablero(ptr, filas, columnas_bits);
+        imprimirTablero(ptr, filas, columnas_bits,tablerofijo);
         unsigned char opcion;
         cout << "ingrese la opcion: ";
         cin >> opcion;
@@ -57,7 +67,7 @@ int main()
         switch(opcion) {
         case 'S':
         case 's':
-            bajar(ptr,filaFicha,columnas_bits,filas);
+            bajar(ptr,tablerofijo,filaFicha,columnas_bits,filas,piezas,columnas,dimensiony,juegoActivo);
             break;
         case 'Q':
         case 'q':
@@ -66,27 +76,26 @@ int main()
             break;
         case 'D':
         case 'd':
-            MoverDerecha(ptr,filaFicha,columnas_bits,filas);
+            MoverDerecha(ptr,filaFicha,columnas_bits,filas,dimensiony);
             break;
         case 'A':
         case 'a':
-            MoverIzquierda(ptr,filaFicha,columnas_bits);
+            MoverIzquierda(ptr,filaFicha,columnas_bits,dimensiony);
             break;
         default:
             cout << "Opcion no valida" << endl;
             break;
 
-
         }
-
-
-
     }
+    cout << "\n GAME OVER !!!\n";
     // liberar memoria
-    for(unsigned int i = 0; i < filas; i++)
+    for(unsigned int i = 0; i < filas; i++){
         delete[] ptr[i];
-
+        delete[] tablerofijo[i];
+    }
     delete[] ptr;
+    delete[] tablerofijo;
 
 
     return 0;
@@ -113,16 +122,26 @@ void inicializarTablero(unsigned char** ptr, unsigned int filas, unsigned int co
     }
 }
 
-void imprimirTablero(unsigned char** ptr, unsigned int filas, unsigned int columnas){
+void imprimirTablero(unsigned char** ptr, unsigned int filas, unsigned int columnas, unsigned char** tablerofijo){
+
+    cout << "\n=======================================================\n";
+    cout << "  _____     _       _      __  __           _   _ \n";
+    cout << " |_   _|___| |_ _ __(_)___|  \\/  | __ _ ___| |_ ___ _ __ \n";
+    cout << "   | |/ _ \\ __| '__| / __| |\\/| |/ _` / __| __/ _ \\ '__|\n";
+    cout << "   | |  __/ |_| |  | \\__ \\ |  | | (_| \\__ \\ ||  __/ |   \n";
+    cout << "   |_|\\___|\\__|_|  |_|___/_|  |_|\\__,_|___/\\__\\___|_|   \n";
+    cout << "=======================================================\n";
 
     for(unsigned int i = 0; i < filas; i++){
         cout << "|";
         for(unsigned int j = 0; j < columnas; j++){
 
+            unsigned char valor = ptr[i][j] | tablerofijo[i][j]; // para superponer ambos tableros el ptr y tablero fijo
 
             for(int bit = 7; bit >=0; bit--){ // es un int normal porque en la ultima iteracion queda 0-1 y ahi se vuelve un overflow y nunca acabaria, un ciclo infinito
 
-                if((ptr[i][j] >> bit) & 1){ // ptr[i][j] lo que significa es tomar el char de esa posicion y tomar los valores bit a bit
+
+                if((valor >> bit) & 1){ // ptr[i][j] lo que significa es tomar el char de esa posicion y tomar los valores bit a bit
 
                     cout << " [ ] ";
                 }
@@ -136,15 +155,44 @@ void imprimirTablero(unsigned char** ptr, unsigned int filas, unsigned int colum
         cout << "|" << endl;
     }
     cout << "Accion: [A]Izq  [D]Der [S]Bajar [W]Rotar [Q]Salir: ";
+
 }
 
-void ingresarFicha(unsigned char** ptr, unsigned char piezas[5][4],unsigned int columnas, unsigned int columnas_bits)
+
+
+void ingresarFicha(unsigned char** ptr, unsigned char piezas[5][4],unsigned int columnas, unsigned int columnas_bits,unsigned int &filaficha,unsigned int &dimensiony,unsigned char** tablerofijo,bool &juegoActivo)
 {
-    unsigned int ficha,mitad = (columnas/2) - 2, contador_mitad = 0; // cuadrado
+    unsigned int ficha,mitad = (columnas/2) - 2;
 
     ficha = rand() % 5;
 
-    for(unsigned int i = 0; i < 4; i++)
+    //dimensiones de la ficha
+    // I
+    if(ficha == 0){
+
+        dimensiony = 4;
+    }
+    //T
+    if(ficha == 1){
+        dimensiony = 2;
+
+    } // esto verifica la altura de la ficha y en que fila esta la parte mas baja de la ficha
+    //z
+    if(ficha == 2){
+        dimensiony = 2;
+
+        //L
+    }
+    if(ficha == 3){
+
+        dimensiony = 3;
+        //O
+    }
+    if(ficha == 4){
+        dimensiony = 2;
+
+    }
+    for(unsigned int i = 0; i < dimensiony; i++)
     {
         if(piezas[ficha][i] != 0)
         {
@@ -154,7 +202,7 @@ void ingresarFicha(unsigned char** ptr, unsigned char piezas[5][4],unsigned int 
     //ingresar en el medio
     for(unsigned int desplazamiento = 0; desplazamiento < mitad; desplazamiento++)//cuantas veces mover la ficha
     {
-        for(unsigned int i = 0; i < 4; i++) // recorrer filas de la ficha
+        for(unsigned int i = 0; i < dimensiony; i++) // recorrer filas de la ficha
         {
             for(int j = columnas_bits - 1; j >= 0; j--)
             {
@@ -168,36 +216,60 @@ void ingresarFicha(unsigned char** ptr, unsigned char piezas[5][4],unsigned int 
             }
         }
     }
+    for(unsigned int i = 0; i < dimensiony; i++)
+    {
+        if((ptr[i][0] & tablerofijo[i][0]) != 0)
+        {
+            juegoActivo = false;
+            return; // game over
+
+        }
+    }
 
 }
 
-void bajar(unsigned char** ptr,unsigned int &filaFicha,unsigned int columnas_bits,unsigned int filas){
+void bajar(unsigned char** ptr,unsigned char** tablerofijo,unsigned int &filaFicha,unsigned int columnas_bits,unsigned int filas, unsigned char piezas[5][4],unsigned int columnas,unsigned int &dimensiony,bool &juegoActivo)
+{
 
 
-    // verifico sie el movimiento es un movimiento posible
-    if (filaFicha + 4 >= filas){
-        cout << "movimiento imposible te excediste del tablero estas excediendo los limites de altura del tablero";
-    }
-
-    else{
-
-        // Mover la ficha hacia abajo
-        for(int i = 3; i >= 0; i--) {
-            for(unsigned int j = 0; j < columnas_bits; j++) {
-
-                ptr[filaFicha + i + 1][j] = ptr[filaFicha + i][j];
+    if(hayColisionAbajo(ptr, tablerofijo, filaFicha, columnas_bits, filas,dimensiony))
+    {
+        // fijar ficha en tablero fijo
+        for(unsigned int i = 0; i < dimensiony; i++)
+        {
+            for(unsigned int j = 0; j < columnas_bits; j++)
+            {
+                tablerofijo[filaFicha + i][j] |= ptr[filaFicha + i][j];
                 ptr[filaFicha + i][j] = 0;
             }
         }
+        //verifico que no este la fila completa
+        borrarFilas(tablerofijo, filas, columnas_bits);
+        // nueva ficha
+        ingresarFicha(ptr,piezas,columnas,columnas_bits,filaFicha,dimensiony,tablerofijo,juegoActivo);
+        filaFicha = 0;//reinicio la posicion inicial
 
-        filaFicha++;
+        return; // hayo una colision entonces termina la funcion
     }
+
+
+    // mover ficha hacia abajo
+    for(int i = (dimensiony - 1); i >= 0; i--)
+    {
+        for(unsigned int j = 0; j < columnas_bits; j++)
+        {
+            ptr[filaFicha + i + 1][j] = ptr[filaFicha + i][j];
+            ptr[filaFicha + i][j] = 0;
+        }
+    }
+
+    filaFicha++;
 }
 
-void MoverDerecha(unsigned char** ptr,unsigned int filaFicha,unsigned int columnas_bits,unsigned int filas){
+void MoverDerecha(unsigned char** ptr,unsigned int filaFicha,unsigned int columnas_bits,unsigned int filas,unsigned int dimensiony){
 
     //aca solo hace una iteracion
-    for(unsigned int i = 0; i < 4; i++) // recorrer filas de la ficha
+    for(unsigned int i = 0; i < dimensiony; i++) // recorrer filas de la ficha
     {
         for(int j = columnas_bits - 1; j >= 0; j--)
         {
@@ -208,21 +280,21 @@ void MoverDerecha(unsigned char** ptr,unsigned int filaFicha,unsigned int column
 
             if(j + 1 < columnas_bits){
                 ptr[i + filaFicha][j+1] |= llevar_bit ;
-                // posicionx++;
+
             }
 
         }
     }
 }
 
-void MoverIzquierda(unsigned char** ptr,unsigned int filaFicha,unsigned int columnas_bits){
+void MoverIzquierda(unsigned char** ptr,unsigned int filaFicha,unsigned int columnas_bits,unsigned int dimensiony){
 
     //aca solo hace una iteracion
-    for(unsigned int i = 0; i < 4; i++) // recorrer filas de la ficha
+    for(unsigned int i = 0; i < dimensiony; i++) // recorrer filas de la ficha
     {
         for(int j = 0; j < columnas_bits; j++)
         {
-            //necesito condiciones para que no haga overflow
+
             unsigned char llevar_bit = (ptr[i + filaFicha][j] & 128) >> 7;
 
             ptr[i+filaFicha][j] <<= 1;
@@ -231,6 +303,64 @@ void MoverIzquierda(unsigned char** ptr,unsigned int filaFicha,unsigned int colu
                 ptr[i + filaFicha][j-1] |= llevar_bit;
             }
 
+        }
+    }
+}
+
+bool hayColisionAbajo(unsigned char** ptr,unsigned char** tablerofijo,unsigned int filaFicha,unsigned int columnas_bits,unsigned int filas, unsigned int dimensiony)
+{
+    for(unsigned int i = 0; i < dimensiony ; i++)
+    {
+        if(filaFicha + i + 1 >= filas)
+            return true;
+
+        for(unsigned int j = 0; j < columnas_bits; j++)
+        {
+            unsigned char ficha = ptr[filaFicha + i][j]; // ficha
+            unsigned char abajo = tablerofijo[filaFicha + i + 1][j]; //tablero fijo para luego hacer un and y comprobar la colision
+
+
+            if((ficha & abajo) != 0)
+                return true;
+        }
+    }
+
+    return false;
+}
+
+void borrarFilas(unsigned char** tablerofijo, unsigned int filas, unsigned int columnas_bits)
+{
+    for(int i = filas - 1; i >= 0; i--)
+    {
+        bool llena = true;
+
+        for(unsigned int j = 0; j < columnas_bits; j++)
+        {
+            if(tablerofijo[i][j] != 255)
+            {
+                llena = false;
+                break;
+            }
+        }
+
+        if(llena)
+        {
+            // bajar todas las filas
+            for(int k = i; k > 0; k--)
+            {
+                for(unsigned int j = 0; j < columnas_bits; j++)
+                {
+                    tablerofijo[k][j] = tablerofijo[k-1][j];
+                }
+            }
+
+            // limpiar la fila de arriba
+            for(unsigned int j = 0; j < columnas_bits; j++)
+            {
+                tablerofijo[0][j] = 0;
+            }
+
+            i++; // volver a revisar la misma fila
         }
     }
 }
